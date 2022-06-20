002ef95085f76d1e442e5b52f950d9721844b5c7
hacenehamroune-semifir <hacene.hamroune@semifir.com>
2022-06-20 20:22:37 +0200

    feat: Demo creation du premier composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+// Import du décorateur 'Component'
 import { Component } from '@angular/core';
 
+// Appel du décorateur @Component.
+// C'est lui qui nous permet de préciser que notre classe est un composant. 
 @Component({
-  selector: 'app-root',
+    // On lui indiquera le nom que l'on donnera au 'sélecteur'. 
+    // Le selecteur est le nom de la balise avec laquelle on doit appeler notre composant
+  selector: 'app-root', // Ici, ce serait <app-root></app-root>
+  // Le templateUrl nous permet d'aller récupérer le fichier html correspondant
   templateUrl: './app.component.html',
+  // StyleUrls nous permet de récupérer la feuille de style de notre composant
+  // Il s'agit bien d'un tableau, ce qui implique que l'on puisse avoir plusieurs feuilles de style.
   styleUrls: ['./app.component.scss']
 })
+
+// Ensuite, la  classe AppComponent est créée.
+// Pour le moment, elle ne contient qu'un title.
 export class AppComponent {
   title = 'demoAngular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+// Imports des modules d'angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { UserComponent } from './components/user/user.component';
+import { CarteVisteComponent } from './components/carte-viste/carte-viste.component';
+import { AlertButtonComponent } from './components/alert-button/alert-button.component';
+import { ListeComponent } from './components/liste/liste.component';
+import { MagieComponent } from './components/magie/magie.component';
+import { ListeFiltreComponent } from './components/liste-filtre/liste-filtre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    UserComponent,
+    CarteVisteComponent,
+    AlertButtonComponent,
+    ListeComponent,
+    MagieComponent,
+    ListeFiltreComponent
   ],
+  //// on déclarera ici les modules à importer
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
