d91c2cc9c927053b38d438d3f80be09e63999388
hacenehamroune-semifir <hacene.hamroune@semifir.com>
2022-06-21 17:00:06 +0200

    feat: Demo pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,13 @@ import { AlertButtonComponent } from './components/alert-button/alert-button.com
 import { ListeComponent } from './components/liste/liste.component';
 import { MagieComponent } from './components/magie/magie.component';
 import { ListeFiltreComponent } from './components/liste-filtre/liste-filtre.component';
+import { UserCardsComponent } from './components/Exercices/exo01/user-cards/user-cards.component';
+import { HidderComponent } from './components/Exercices/exo01/hidder/hidder.component';
+import { ShowDateComponent } from './components/show-date/show-date.component';
+import { GenericAlertButtonComponent } from './components/generic-alert-button/generic-alert-button.component';
+import { ButtonMenuComponent } from './components/button-menu/button-menu.component';
+import { HorlogeComponent } from './components/Exercices/exo02/horloge/horloge.component';
+import { ClockMenuComponent } from './components/Exercices/exo02/clock-menu/clock-menu.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,14 @@ import { ListeFiltreComponent } from './components/liste-filtre/liste-filtre.com
     AlertButtonComponent,
     ListeComponent,
     MagieComponent,
-    ListeFiltreComponent
+    ListeFiltreComponent,
+    UserCardsComponent,
+    HidderComponent,
+    ShowDateComponent,
+    GenericAlertButtonComponent,
+    ButtonMenuComponent,
+    HorlogeComponent,
+    ClockMenuComponent
   ],
   //// on déclarera ici les modules à importer
   imports: [
