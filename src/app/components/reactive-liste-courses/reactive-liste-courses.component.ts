b856267ed2033b22739a0421d4b746c216106e57
hacenehamroune-semifir <hacene.hamroune@semifir.com>
2022-06-22 10:53:11 +0200

    feat: Demo reactive fomrs

---
+++ src/app/components/reactive-liste-courses/reactive-liste-courses.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-liste-courses',
+  templateUrl: './reactive-liste-courses.component.html',
+  styleUrls: ['./reactive-liste-courses.component.scss']
+})
+export class ReactiveListeCoursesComponent implements OnInit {
+
+  article: FormGroup;
+
+  articles: any[] = [];
+  constructor() {
+    this.article= new FormGroup({
+      designation: new FormControl(''),
+      prix: new FormControl('')
+    });
+   }
+
+  ngOnInit(): void {
+  }
+
+
+  addArticle() {
+    this.articles.push(this.article.value)
+    this.article.reset();
+  }
+
+  get totalPrice(): number {
+    return this.articles.reduce((total, article)=> total + article.prix, 0 )
+  }
+}
