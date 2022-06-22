c1e853024aa6ef024f36e69c3032dda5e22cb0cd
hacenehamroune-semifir <hacene.hamroune@semifir.com>
2022-06-22 17:16:34 +0200

    feat: Demo formArrays

--- src/app/components/Exercices/exo03/user-form/user-form.component.ts
+++ src/app/components/Exercices/exo03/user-form/user-form.component.ts
@@ -7,58 +7,63 @@ import { Form, FormArray, FormBuilder, FormGroup, Validators } from '@angular/fo
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
- // Déclaration du tableau utilisateurs, avec un utilisateur exemple.
- users: any[] = [
-  {
-    nom: 'Norris',
-    prenom: 'Chuck',
-    email: 'chuck.norris@dieu.com',
-    telephone: '0123456789',
-    entreprise: 'GOD & Cie',
-  },
-];
+  users: any[] = [
+    {
+      nom: 'Norris',
+      prenom: 'Chuck',
+      email: 'chuck.norris@dieu.com',
+      telephones: ['0123456789', '0123456789'],
+      entreprise: 'GOD & Cie',
+    },
+  ];
 
-// Déclaration du formulaire
-userForm: FormGroup = this.formBuilder.group({
-  // Attribut nom avec un validateur 'required' et une longueur minimale de 2
-  nom: ['', [ Validators.minLength(2), Validators.required]],
-  // Attribut prenom avec un validateur 'required' et une longueur minimale de 2
-  prenom: ['', [Validators.minLength(2), Validators.required]],
-  // Attribut email avec un validateur de type email
-  email: ['', [Validators.email, Validators.required ]],
-  // Attribut telephone avec un validateur 'required' et une longueur minimale de 10
-  telephone: ['', [Validators.minLength(10), Validators.required]],
-  // Attribut entreprise avec un validateur 'required' et une longueur minimale de 2
-  entreprise: ['', [Validators.minLength(2), Validators.required]],
-});
+  userForm: FormGroup = this.formBuilder.group({
+    nom: ['', [ Validators.minLength(2), Validators.required]],
+    prenom: ['', [Validators.minLength(2), Validators.required]],
+    email: ['', [Validators.email, Validators.required ]],
+    // On déclare ici un tableau de FormArray
+    // Dans lequel on ajoute un control pour un téléphone
+    telephones: this.formBuilder.array([
+      this.formBuilder.control('', [Validators.minLength(10), Validators.required]),
+    ]),
+    entreprise: ['', [Validators.minLength(2), Validators.required]],
+  });
 
-// Définition d'un bool avec une valeur par défaut à false
-// Servira à s'assurer de la soumussion du formulaire
-submitted: boolean = false;
+  submitted: boolean = false;
 
-// Ajout du formulaire dans le constructeur
-constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {}
 
-ngOnInit(): void {}
+  ngOnInit(): void {}
 
-// Méthode pour ajotuer un utilisateur
-// Elle est privée et sera appellée par la méthode onSubmit
-private addUser(): void {
-  this.users.push(this.userForm.value);
-  this.userForm.reset();
-  this.submitted = false;
-}
-
-// Méthode onSubmit pour gérer la soumission du formulaire
-public onSubmit(): void {
-  this.submitted = true
-  if (this.userForm.valid) {
-    this.addUser();
+  private addUser(): void {
+    this.users.push(this.userForm.value);
+    this.userForm.reset();
+    this.submitted = false;
   }
-}
+  public onSubmit(): void {
+    this.submitted = true
+    if (this.userForm.valid) {
+      this.addUser();
+    }
+  }
+  public get form() {
+    return this.userForm.controls;
+  }
 
-// Getter pour accéder au controls du formulaire
-public get form() {
-  return this.userForm.controls;
-}
+// Getter pour accéder à la liste des téléphones
+  public get telephones(): FormArray {
+    return this.userForm.get('telephones') as FormArray;
+  }
+  // Méthode pour ajouter un control de téléphone
+  // La méthode va push un control de téléphone dans le tableau 'téléphones'
+  public addTelephone(): void {
+    this.telephones.push(this.formBuilder.control('', [Validators.minLength(10), Validators.required]));
+  }
+  // Méthode pour supprimer un control de téléphone
+  // On retire le dernier élément de l'index
+  // NB le compte commence à 1, l'index commence à 0 !
+  public removeTelephone(): void {
+    this.telephones.removeAt(this.telephones.length - 1);
+  }
+
 }
